a85edbcba3a8922ebe39e8e812e2b037eb918874
baserrato <benjamins@liatrio.com>
2022-11-08 13:21:05 -0800

    build: recognizes resource engineer as part of terraform plan

---
+++ examples/provider/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    devops-bootcamp = {
+      source = "liatr.io/terraform/devops-bootcamp"
+    }
+  }
+}
+
+provider "devops-bootcamp" {
+  endpoint = "http://localhost:8080"
+}
+
+resource "devops-bootcamp_engineer" "bob" {
+  name  = "bob"
+  email = "bob@bob.com"
+}
