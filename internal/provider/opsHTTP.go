e094401eb52b9f2254d1d28eee1d27d3a01187a5
Steven Mendez <96746466+MinecraftSt3v3@users.noreply.github.com>
2022-11-16 15:49:26 -0800

    Update opsHTTP.go

--- internal/provider/opsHTTP.go
+++ internal/provider/opsHTTP.go
@@ -35,6 +35,31 @@ func (c *Client) GetOps() ([]Ops_Api, error) {
 	return ops, nil
 }
 
+func (c *Client) UpdateOps(ops Ops_Api) (*Ops_Api, error) {
+	rb, err := json.Marshal(ops)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/ops/", c.HostURL), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	newOps := Ops_Api{}
+	err = json.Unmarshal(body, &newOps)
+	if err != nil {
+		return nil, err
+	}
+
+	return &newOps, nil
+}
+
 func (c *Client) CreateOp(ops Ops_Api) (*Ops_Api, error) {
 	rb, err := json.Marshal(ops)
 	if err != nil {
@@ -58,4 +83,18 @@ func (c *Client) CreateOp(ops Ops_Api) (*Ops_Api, error) {
 	}
 
 	return &newOp, nil
+}
+
+func (c *Client) DeleteOps(Id string) error {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/ops/%s", c.HostURL, Id), nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.doRequest(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
