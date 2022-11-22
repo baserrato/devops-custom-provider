83e41a70c8b4f20d70e2c9e3245b61d921e8afa7
rhoofard <42755382+rhoofard@users.noreply.github.com>
2022-11-22 15:42:45 -0800

    fixed devops_resource so that it correctly deletes state when it reads something that isnt there

--- internal/provider/devops_resource.go
+++ internal/provider/devops_resource.go
@@ -225,12 +225,8 @@ func (r *DevOpsResource) Read(ctx context.Context, req resource.ReadRequest, res
 	}
 
 	newDevOps, err := r.client.GetDevOp(state.Id.ValueString())
-
 	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error creating dev",
-			"Could not create dev, unexpected error: "+err.Error(),
-		)
+		resp.State.RemoveResource(ctx)
 		return
 	}
 
